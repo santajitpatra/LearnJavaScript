b6f0ccd8eb8973a48d8fa995d24e72128c28a32d
Santajit Patra <business.santajitpatra@gmail.com>
2023-08-21 23:59:45 +0530

    feat : promises
    feat : promises

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("kk")
+console.log("Hi, There");
+
+// right way to get the data promises by async function
+async function getData() {
+    let data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    console.log(await data.json());
+}
+getData();
+
+// 2nd way to get the data (wrong way)
+fetch("https://jsonplaceholder.typicode.com/posts").then((data) =>{
+    console.log(data);
+} ).catch((error) => {
+    console.log(error);
+})
