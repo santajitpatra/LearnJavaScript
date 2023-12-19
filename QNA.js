d6ec05a9d48e60ac34f97e4057fa8c967bf3ee67
Santajit Patra <business.santajitpatra@gmail.com>
2023-12-19 22:15:58 +0530

    learn : continue method
    learn : continue method

--- QNA.js
+++ QNA.js
@@ -1,11 +1,17 @@
-// what are (High order function)
-// function (fn) {return fn} 
+// 2.. continue method
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 == 0) continue;
+  console.log(`continue = ${i}`);
+}
+
+//1.. what are (High order function)
+// function (fn) {return fn}
 function multiplyBy(arg) {
   return function (number) {
     return number * arg;
   };
 }
 
-let multiplyByTwo = multiplyBy(2)
-let res = multiplyByTwo(50)
-console.log(`HOF = ${res}`)
+let multiplyByTwo = multiplyBy(2);
+let res = multiplyByTwo(50);
+console.log(`HOF = ${res}`);
